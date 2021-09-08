7f1cc473844100f30da23d09f6f6a7d27680ac9c
Mario S. Alves <malves.dev@gmail.com>
2021-09-08 16:21:10 -0300

    Adj:Tradução de mensagens do teste

--- tests/update.js
+++ tests/update.js
@@ -2,34 +2,36 @@
  * http://usejsdoc.org/
  */
 const CronJobManager = require('../lib/crontab_manager'),
-cronTab = new CronJobManager();
+let cronTab = new CronJobManager();
 
 exports.test = () => {
-    let currDate = new Date();
-  cronTab.add('updateTest', currDate, () => {console.log("update Cron... wooo!")});
-  //update the tab - string
+  let currDate = new Date();
+  cronTab.add('updateTest', currDate, () => {
+    console.log("atualizar Cron ... wooo!")
+  });
+    
+  // Atualize a guia - string
   let tabString = '0 */2 * * * *';
   cronTab.update('updateTest', tabString);
-  console.assert(/0 \*\/2 \* \* \* \*/.test(cronTab), `Could not find updated crontab String! ${cronTab}`);
+  console.assert(/0 \*\/2 \* \* \* \*/.test(cronTab), `Não foi possível encontrar string crontab atualizada! ${cronTab}`);
   
-  //update the tab - Date;
+  // Atualiza o guia Data;
   let testDate = new Date('2018-08-24T09:57:22-0400');
   cronTab.update('updateTest', testDate);
   console.assert(/Fri Aug 24 2018 09\:57\:22 GMT-0400/.test(cronTab), `Could not find updated Crontab date! ${cronTab}`);
   
-  //update the task
+  // Atualiza a tarefa
   let newFunc = function() {
-      
-      console.log("you can now doo a double wooo... wooo wooo....");
+    console.log("Agora você pode fazer um duplo wooo... wooo wooo ....");
   }
   
   cronTab.update('updateTest', newFunc);
-  console.assert(`${cronTab}`.search("double wooo"), "coulnd't the update task when updating just the task!");
+  console.assert(`${cronTab}`.search("double wooo"), "Não consigo a tarefa de atualização ao atualizar apenas a tarefa!");
   
-  //update the tab and the task - we know the update tab works. We just need to make sure the task gets updated.
+  // Atualize a guia e a tarefa - sabemos que a guia de atualização funciona. Só precisamos ter certeza de que a tarefa será atualizada
   cronTab.update('updateTest', testDate, () => {console.log("A New Task!")});
   
-  console.assert(`${cronTab}`.search("A New Task!"), "could't find the updated task when updating tab and task!");
+  console.assert(`${cronTab}`.search("A New Task!"), "Não foi possível encontrar a tarefa atualizada ao atualizar a guia e a tarefa!");
    
   
 };
