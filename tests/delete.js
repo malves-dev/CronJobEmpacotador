09e39e5ef1f7dff88653f07fdfdbdacbab293007
Mario S. Alves <malves.dev@gmail.com>
2021-09-08 16:26:48 -0300

    ADJ: Tradução de mensagens

--- tests/delete.js
+++ tests/delete.js
@@ -5,7 +5,7 @@
 var CronJobManager = require('../lib/crontab_manager');
 exports.test = () => {
     let date = new Date();
-    crontab1 = new CronJobManager('deleteMe', date, () =>{console.log("delete me...")} );
+    crontab1 = new CronJobManager('deleteMe', date, () =>{console.log("Me apague...")} );
     crontab1.deleteJob('deleteMe');
-    console.assert(! crontab1.jobs.deleteMe, "The deleteMe job was not deleted!!! %s", crontab1);
+    console.assert(!crontab1.jobs.deleteMe, "O trabalho deleteMe não foi excluído! %s", crontab1);
 }
